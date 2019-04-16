2c09f8aff05805bc5dc13a41017332f476d96296
niu69324 <693245832@qq.com>
2019-04-16 15:55:59 +0800

    Create tx.py

---
+++ niu/python3/tx.py
@@ -0,0 +1,33 @@
+#生成器generator
+gt=(x*x for x in range(1,15))
+print(gt)
+#print(next(gt))
+print('============')
+
+for x in gt:
+	print(x)
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+print(fib(10))
+
+	
+#杨辉三角
+def yh(n):
+	li=[1]
+	index=0
+	while index<n:
+		yield li
+		li=[1]+[li[i] + li[i + 1] for i in range(len(li) - 1)]+[1]
+		index+=1
+
+for x in yh(10):
+	print(x)
+	
